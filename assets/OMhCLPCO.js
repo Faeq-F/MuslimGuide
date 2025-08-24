64e99bdf051a4ba93bbde7d3b6cf8f55a1c4f43a
Faeq-F <faeqfaisal@hotmail.co.uk>
2025-08-24 17:22:37 +0000

    Deploying to gh-pages from  @ 75f9878bc9f78ebe740b57d14582448b39e3f1cf 🚀

---
+++ assets/OMhCLPCO.js
@@ -0,0 +1,32 @@
+import{_ as e}from"./DlAUqK2U.js";import{c as r,a as l,d as t,F as i,o}from"./D4RPe2bu.js";const a={};function u(s,n){return o(),r(i,null,[n[0]||(n[0]=l("ul",null,[l("li",null,[t(`
+
+      Pray Maghrib & Isha together at Muzdalifah with one azaan.
+
+      `),l("br"),t(`
+
+      (Shorten & Combined).
+
+    `)]),t(),l("li",null,`
+
+      In Muzdalifah or Mina, you can collect upto 70 pebbles for stoning.
+
+    `),t(),l("li",null,`
+
+      Spend the night in Muzadlifah (sleep) untill Fajr.
+
+    `)],-1)),n[1]||(n[1]=t(`
+
+  Note:`)),n[2]||(n[2]=l("br",null,null,-1)),n[3]||(n[3]=t(`
+
+  At Muzdalifah, you will stay under the night sky.
+
+  `)),n[4]||(n[4]=l("br",null,null,-1)),n[5]||(n[5]=t(`
+
+  There are no tents or other accommodation facilities here.
+
+  `)),n[6]||(n[6]=l("br",null,null,-1)),n[7]||(n[7]=l("br",null,null,-1)),n[8]||(n[8]=t(`
+
+  Although there are plenty of lights, it is still fairly dark. Toilets and
+  Wudhu facilities are available in Muzdalifah, but they will be crowded.
+
+`))],64)}const b=e(a,[["render",u]]);export{b as default};
---
+++ assets/QoYMibVt.js
@@ -0,0 +1,21 @@
+import{_ as r}from"./DlAUqK2U.js";import{c as t,a as l,d as a,F as e,o}from"./D4RPe2bu.js";const u={};function s(f,n){return o(),t(e,null,[n[0]||(n[0]=l("ul",null,[l("li",null,`
+
+      If you didn’t leave Mina on the 12th, then on the 13th stone all 3 Jamarat
+      with 7 pebbles for each Jamarat.
+
+    `),a(),l("li",null,`
+
+      Leave for Makkah.
+
+    `),a(),l("li",null,`
+
+      Before final departure from Makkah, perform Tawaaful- Wadaa' (Farewell
+      Tawaf) as your last act.
+
+    `)],-1)),n[1]||(n[1]=a()),n[2]||(n[2]=l("br",null,null,-1)),n[3]||(n[3]=l("br",null,null,-1)),n[4]||(n[4]=a(`
+
+  Congratulations!`)),n[5]||(n[5]=l("br",null,null,-1)),n[6]||(n[6]=a(`
+
+  Your Hajj is Complete!
+
+`))],64)}const d=r(u,[["render",s]]);export{d as default};
