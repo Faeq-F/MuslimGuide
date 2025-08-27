29c6a63321cf5742fe65e4cbcfdc35c9ecf1202e
Faeq-F <faeqfaisal@hotmail.co.uk>
2025-08-27 17:03:49 +0000

    Deploying to gh-pages from  @ 509a25b00452b58b49f1c0c336427e95ae2cfb4f 🚀

---
+++ assets/CvAHfnFj.js
@@ -0,0 +1,26 @@
+import{_ as a}from"./DlAUqK2U.js";import{c as r,S as n,o}from"./CeEg2OAF.js";const d={};function t(i,e){return o(),r("div",null,e[0]||(e[0]=[n(`<div class="p-4 m-4 mb-2 border border-[#e7edf4] dark:border-[#223649] rounded-2xl"><div class="font-bold mb-2 underline decoration-1 decoration-[#e7edf4] dark:decoration-[#223649] underline-offset-4">
+        1
+      </div>
+      If Tawaf Ul-Ifaadhah has still not been performed, go to Makkah and
+      perform Tawaf. Pray 2 rakaats, drink from Zam Zam &amp; perform Sa’ee.
+      <br><br>
+      Return back to Mina.
+    </div> <div class="p-4 m-4 mb-2 border border-[#e7edf4] dark:border-[#223649] rounded-2xl"><div class="font-bold mb-2 underline decoration-1 decoration-[#e7edf4] dark:decoration-[#223649] underline-offset-4">
+        2
+      </div>
+      From the time period between after Zawaal (after midday) until the
+      night, stone all 3 Jamarat with 7 pebbles for each one.
+      <br><br>
+      Leave Mina for Makkah before Sunset if possible. If you can’t leave -
+      Stay at Mina.
+    </div> <div class="p-4 m-4 mb-2 border border-[#e7edf4] dark:border-[#223649] rounded-2xl"><div class="font-bold mb-2 underline decoration-1 decoration-[#e7edf4] dark:decoration-[#223649] underline-offset-4">
+        3
+      </div>
+      A pilgrim can leave grounds of Mina before &#39;MAGRIB&#39; and end the Hajj
+      Journey as mentioned in the Quran.
+      <br><br>
+      If you decide to leave then reach Makkah and before the final departure,
+      perform Tawaaful- Wadaa&#39; (Farewell Tawaf) as your last act.
+      <br><br> <b>Note:</b> If you have a question, please consult a SHAYKH/ MOLANA/
+      GUIDE.
+    </div>`,5)]))}const s=a(d,[["render",t]]);export{s as default};
---
+++ assets/NzFz-XS9.js
@@ -0,0 +1,1 @@
+import"./CeEg2OAF.js";const e=""+new URL("kaaba-key-features.9S6tPmW4.jpg",import.meta.url).href;export{e as k};
